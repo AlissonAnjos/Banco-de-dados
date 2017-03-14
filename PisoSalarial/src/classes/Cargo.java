07c5149ec1cc31fd1470df490a9ee06db8875936
AlissonAnjos <alissontsukuyomi@gmail.com>
2017-03-14 12:06:45 -0300

    Update Cargo.java

--- PisoSalarial/src/classes/Cargo.java
+++ PisoSalarial/src/classes/Cargo.java
@@ -2,6 +2,7 @@ package classes;
 
 public class Cargo {
 	private String descricao;
+	private Integer id;
 
 	public String getDescricao() {
 		return descricao;
@@ -9,5 +10,12 @@ public class Cargo {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 }
