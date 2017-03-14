3f3f4e93151bdbd6d05dd0794ce2d02447579d50
AlissonAnjos <alissontsukuyomi@gmail.com>
2017-03-14 12:08:27 -0300

    Update Classe.java

--- PisoSalarial/src/classes/Classe.java
+++ PisoSalarial/src/classes/Classe.java
@@ -1,15 +1,23 @@
 package classes;
 
 public class Classe {
-
+	private Integer id;
 	private String sigla;
 	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 	public String getSigla() {
 		return sigla;
 	}
 	public void setSigla(String sigla) {
 		this.sigla = sigla;
 	}
+	
 	
 	
 }
